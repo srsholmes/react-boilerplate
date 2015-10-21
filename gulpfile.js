fca5b7c2f2d8c48b5b0fd7379f460bb07349ae91
srsholmes <srsholmes@gmail.com>
2015-10-21 10:51:21 +0100

    live reload not on prod

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ var isProd = process.env.NODE_ENV === "production"
 var bundler = browserify({
   entries:      [config.sourceDir + 'js/app.js'],
   transform:    babelify.configure({stage: 0}),
-  plugin:       [isProd ? [] : [ lrload ]],
+  plugin:       isProd ? [] : [ lrload ],
   debug:        !isProd,
   cache:        {},
   packageCache: {},
